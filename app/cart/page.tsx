81642a871ced8512194ba7ccf631aa193b543cd2
Chirasi Amaya Walpola <2020cs197@stu.ucsc.cmb.ac.lk>
2023-12-28 18:32:22 +0530

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from '../components/Container';
 import CardClient from './CartClient';
+import { getCurrentUser } from '@/actions/getCurrentUser';
 
-const Cart = () => {
+const Cart = async() => {
+    const currentUser = await getCurrentUser();
+
   return (
     <div className='pt-8'>
       <Container>
-        <CardClient />
+                <CardClient currentUser ={currentUser} />
       </Container>
     </div>
   )
